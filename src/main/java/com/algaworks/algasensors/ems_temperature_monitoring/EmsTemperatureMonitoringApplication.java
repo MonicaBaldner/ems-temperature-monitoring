441c138135269c7fad3b167957600f7bcbc26ac7
Mônica Baldner <baldnermonica@gmail.com>
2025-05-26 16:55:08 -0300

    New project files

---
+++ src/main/java/com/algaworks/algasensors/ems_temperature_monitoring/EmsTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.ems_temperature_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsTemperatureMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algasensors/ems_temperature_monitoring/EmsTemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.ems_temperature_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsTemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
